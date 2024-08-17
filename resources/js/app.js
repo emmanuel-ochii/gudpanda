5b90886394d320587e6aa3a02aa7a0afbe450079
emmanuel-ochii <emmanuelochubili@gmail.com>
2024-08-17 16:19:50 +0100

    replace the relative path with @  "jsconfig" helper

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
-import  FrontendLayout from './Layouts/FrontendLayout.vue'
+import  FrontendLayout from '@/Layouts/FrontendLayout.vue'
 
 createInertiaApp({
     resolve: name => {
